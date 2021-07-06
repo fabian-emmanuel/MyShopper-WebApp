6861c3d430c9f37c8637e3cee5f938082cbdcf68
FABIAN EMMANUEL <emmanuel.fabian@decagon.dev>
2021-07-06 14:22:08 +0100

    frontend setup refactor

--- MyShopperWebParent/MyShopperFrontend/src/main/java/com/codewithfibbee/myshopperfrontend/MyShopperFrontendApplication.java
+++ MyShopperWebParent/MyShopperFrontend/src/main/java/com/codewithfibbee/myshopperfrontend/MyShopperFrontendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyshopperfrontendApplication {
+public class MyShopperFrontendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyshopperfrontendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyShopperFrontendApplication.class, args);
+    }
 
 }
--- MyShopperWebParent/MyShopperFrontend/src/test/java/com/codewithfibbee/myshopperfrontend/MyShopperFrontendApplicationTests.java
+++ MyShopperWebParent/MyShopperFrontend/src/test/java/com/codewithfibbee/myshopperfrontend/MyShopperFrontendApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyshopperfrontendApplicationTests {
+class MyShopperFrontendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
