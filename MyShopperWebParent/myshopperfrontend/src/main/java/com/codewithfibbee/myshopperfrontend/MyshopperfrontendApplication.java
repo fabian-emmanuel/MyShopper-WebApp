b0d82c9d0102326a26d3fab5aeff334d7ab0a2ef
FABIAN EMMANUEL <emmanuel.fabian@decagon.dev>
2021-07-06 14:00:48 +0100

    frontend setup

---
+++ MyShopperWebParent/myshopperfrontend/src/main/java/com/codewithfibbee/myshopperfrontend/MyshopperfrontendApplication.java
@@ -0,0 +1,13 @@
+package com.codewithfibbee.myshopperfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshopperfrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshopperfrontendApplication.class, args);
+	}
+
+}
---
+++ MyShopperWebParent/myshopperfrontend/src/test/java/com/codewithfibbee/myshopperfrontend/MyshopperfrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithfibbee.myshopperfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshopperfrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
